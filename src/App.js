71fd16665f0013318eff22e9204a49e7c493ae64
lamiamostafa <lamiamostafa791@gmail.com>
2022-05-09 04:19:59 +0600

    banner part is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import About from './Pages/About/About';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
+import Blogs from './Pages/Home/Blogs/Blogs';
+import Header from './Pages/Shared/Header/Header';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
+import NotFound from './Pages/Shared/NotFound/NotFound';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
+import Footer from './Pages/Shared/Footer/Footer';
+import Checkout from './Pages/Checkout/Checkout/Checkout';
+import Feedback from 'react-bootstrap/esm/Feedback';
+import WhyUs from './Pages/Home/WhyUs/WhyUs';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="feedback" element={<Feedback></Feedback>}></Route>
+        <Route path="whyUS" element={<WhyUs></WhyUs>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/checkout" element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+
+      </Routes>
+      <Footer></Footer>
+
 
     </div>
   );
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,58 +1,25 @@
-import React, { useState } from 'react';
-import { Carousel } from 'react-bootstrap';
+import React from 'react';
+// import { Carousel } from 'react-bootstrap';
 import './Banner.css';
 
-import banner1 from '../../../images/banner/slider3.png';
-import banner2 from '../../../images/banner/slider2.png';
-import banner3 from '../../../images/banner/slider3.png';
+import banner1 from '../../../images/category/home-img.png';
+
 
 const Banner = () => {
-    const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-    };
     return (
 
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-                <img
-                    className=" d-block slide-img"
-                    src={banner1}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>Every Patients id Different</h3>
-                    <p>Every Smile is Unique</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block slide-img"
-                    src={banner2}
-                    alt="Second slide"
-                />
+        <section class="banner-section">
+            <div class="image">
+                <img className="img-fluid" src={banner1}></img>
 
-                <Carousel.Caption>
-                    <h3>We are here to serve you</h3>
-                    <p>You don't have to worry now.Our 24 hour service is available now</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block  slide-img"
-                    src={banner3}
-                    alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                    <h3>We have Best Medicare plan option for you</h3>
-                    <p>
-                        We are Ready to Serve you the best treatment.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+            </div>
+            <div className="content">
+                <span>Fresh And Organic</span>
+                <h3>Your Daily Need Products</h3>
+                <button className="btn">GetStarted</button>
+            </div>
+        </section>
     );
 };
 
