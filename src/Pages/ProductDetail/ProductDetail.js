d78d3c71682ba9c3ec2b8a535cee5ffeaf42481f
lamiamostafa <lamiamostafa791@gmail.com>
2022-05-12 15:54:26 +0600

    data load in productDetails

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
     const { productId } = useParams();
+    const [product, setProduct] = useState({});
+    useEffect(() => {
+        const url = `http://localhost:5000/product/${productId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setProduct(data));
+    })
     return (
         <div>
-
+            <h2>You are about to book: {product.name}</h2>
             <div className='text-center'>
                 <Link to="/manageallitems">
                     <button className='btn btn-success my-5'>Proceed to Inventory</button>
