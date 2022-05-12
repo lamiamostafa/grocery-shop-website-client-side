f18816ad87350b2fab690b8a043369c539bf846f
lamiamostafa <lamiamostafa791@gmail.com>
2022-05-12 18:02:18 +0600

    email verification set up in inventory page

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,11 +1,14 @@
+import { sendEmailVerification } from 'firebase/auth';
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
 const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const location = useLocation();
     if (loading) {
         return <Loading></Loading>
@@ -13,7 +16,24 @@ const RequireAuth = ({ children }) => {
     if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
-    return children;
+    if (user.providerData[0]?.providerid === 'password' && !user.emailVerified) {
+        return <div className="text-center mt-5">
+            <h3 className="text-danger">Your Email is not verified</h3>
+            <h5 className="text-success">Please verify your email address</h5>
+            <button
+                className="btn btn-primary"
+                onClick={async () => {
+                    await sendEmailVerification();
+                    toast('Sent email');
+
+                }}>
+                send again Verify email
+
+            </button>
+            <ToastContainer></ToastContainer>
+
+        </div>
+    }
 };
 
 export default RequireAuth;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -28,6 +28,7 @@ const Header = () => {
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
 
+                            <Nav.Link href="home">Home</Nav.Link>
                             <Nav.Link href="home#services">Services</Nav.Link>
                             <Nav.Link href="home#whyUs">Why Us</Nav.Link>
                             <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
